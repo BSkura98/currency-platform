9eb8e453bb616d4f373f33d4a057bcd439d92336
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2025-02-12 08:47:29 +0100

    Implement withdraw

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 
 import sequelize from "./database/database";
+import { setExchangeRateToPLN } from "./services/setExchangeRateToPLN/service";
+import { setOperationTypeCommission } from "./services/setOperationTypeCommission/service";
+import { deposit } from "./services/deposit/service";
 
 dotenv.config();
 
 sequelize.sync().then(async () => {
   console.log("Database is ready");
+
+  setExchangeRateToPLN(3, 4.06);
+  setOperationTypeCommission(3, 0.04);
+  deposit(1, "EUR", 5.3);
 });
