f9867311690ff3d22de0d5004ce7bfb58ffacadf
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2025-02-16 19:30:39 +0100

    Change order of parameters in commands

--- src/userInput/commandHandlers/deposit.ts
+++ src/userInput/commandHandlers/deposit.ts
@@ -2,7 +2,7 @@ import { deposit } from "../../services/Account/deposit/service";
 
 export const depositHandler = async (args: string[]) => {
   try {
-    await deposit(Number(args[1]), Number(args[2]), args[3]);
+    await deposit(Number(args[1]), Number(args[3]), args[2]);
     console.log("Deposit successful");
   } catch (error: any) {
     console.log(error.message);
--- src/userInput/commandHandlers/withdraw.ts
+++ src/userInput/commandHandlers/withdraw.ts
@@ -2,7 +2,7 @@ import { withdraw } from "../../services/Account/withdraw/service";
 
 export const withdrawHandler = async (args: string[]) => {
   try {
-    await withdraw(Number(args[1]), Number(args[2]), args[3]);
+    await withdraw(Number(args[1]), Number(args[3]), args[2]);
     console.log("Withdrawal successful");
   } catch (error: any) {
     console.log(error.message);
